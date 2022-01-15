4fc59e01e48351b3a9774f46e81eaec64c04c0fd
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2022-01-15 06:22:40 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
 
   getVMDetails(serverType:IServer, vm: MachineList ) {
-   let sum = vm.map( (server) => server.HDD)
-      .reduce( (a, b) => a + b, 0);
-   if( sum >= serverType.HDD ) {
-     return;
-   }
-    return {
-      "message": "Number of vms the user can use",
-      "number of VMs" : serverType.result+=1
+    try {
+      let sum = vm.map((server) => server.HDD)
+        .reduce((a, b) => a + b, 0);
+      if (sum >= serverType.HDD) {
+        return;
+      }
+      return {
+        'message': 'Number of vms the user can use',
+        'number of VMs': serverType.result += 1,
+      };
+    } catch (e) {
+      throw new HttpException('BadRequestException', HttpStatus.BAD_REQUEST)
     }
   }
 }
